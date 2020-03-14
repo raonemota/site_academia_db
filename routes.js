9c6cac8745dd80f3260538b654c7dc574e500d5c
raonemota <raonemota@hotmail.com>
2020-03-14 20:09:42 -0300

    Created page create instructor

--- routes.js
+++ routes.js
@@ -2,7 +2,23 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get("/", function(req, res){  
-    return res.render("about", {item: data})
+    return res.redirect("/instructors")
+})
+
+routes.get("/instructors", function(req, res){  
+    return res.render("instructors/index")
+})
+
+routes.get("/instructors/create", function(req, res){
+    return res.render("instructors/create")
+})
+
+routes.post("/instructors", function(req, res){
+    return res.send("Recebido")
+})
+
+routes.get("/members", function(req, res){  
+    return res.render("members")
 })
 
 module.exports = routes
